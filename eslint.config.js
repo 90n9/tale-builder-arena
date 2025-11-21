26d470e57d1d18c2c07cfaaab42489f7d5534b66
90n9 <tholop@gmail.com>
2025-11-21 20:10:15 +0700

    fix build warning

--- eslint.config.js
+++ eslint.config.js
@@ -4,24 +4,35 @@ import reactHooks from "eslint-plugin-react-hooks";
 import nextPlugin from "@next/eslint-plugin-next";
 import tseslint from "typescript-eslint";
 
-const nextRules = nextPlugin.configs.recommended?.rules ?? {};
+const nextRecommendedRules = nextPlugin.configs.recommended?.rules ?? {};
+const nextCoreWebVitalsRules = {
+  ...nextRecommendedRules,
+  "@next/next/no-html-link-for-pages": "error",
+  "@next/next/no-sync-scripts": "error",
+};
 
 export default tseslint.config(
   { ignores: ["dist", ".next"] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    plugins: {
+      "@next/next": nextPlugin,
+    },
+  },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
     files: ["**/*.{ts,tsx}"],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
     },
     plugins: {
-      "react-hooks": reactHooks,
       "@next/next": nextPlugin,
+      "react-hooks": reactHooks,
     },
     rules: {
+      ...nextCoreWebVitalsRules,
       ...reactHooks.configs.recommended.rules,
-      ...nextRules,
       "@typescript-eslint/no-unused-vars": "off",
       "@typescript-eslint/triple-slash-reference": "off",
     },
